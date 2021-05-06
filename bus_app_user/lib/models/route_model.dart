0494554191a8d2b387f10ff459b815263c1ec5b0
AFzAL <afzl.wtu@hotmail.com>
2021-05-06 13:13:07 +0500

    imported model and delted temp

---
+++ bus_app_user/lib/models/route_model.dart
@@ -0,0 +1,8 @@
+class RouteModel {
+  final String from;
+  final String to;
+  final DateTime fromTime;
+  final DateTime toTime;
+
+  RouteModel({this.fromTime, this.toTime, this.from, this.to});
+}
